09f71353dac6380e2f08a4c3476aa2b4df658327
Adriano Maurmann <adriano.maurmann@gmail.com>
2023-06-29 23:32:28 -0300

    first commit

---
+++ Database/DML_Team.sql
@@ -0,0 +1,7 @@
+SELECT * from Team
+
+insert into team(name) values('Tomahawk');
+insert into team(name) values('Phantom');
+insert into team(name) values('Nintendo');
+insert into team(name) values('Estrela da Morte');
+insert into team(name) values('Ninjas');
